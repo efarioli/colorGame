d15a4a8fa9ed86d386a43e7e6cf54668c4fb6724
zeke farioli <efarioli@gmail.com>
2017-03-04 21:37:00 +0000

    add event listener to every square and see if match with picked color

--- colorGame.js
+++ colorGame.js
@@ -1,10 +1,10 @@
 var colors = [
-  "rgb(255,0,0)",
-  "rgb(255,255,0)",
-  "rgb(0,255,0)",
-  "rgb(0,255,255)",
-  "rgb(0,0,255)",
-  "rgb(255,0,255)"
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 0, 255)"
 ];
 
 var squares = document.querySelectorAll(".square");
@@ -13,6 +13,21 @@ var colorDisplay = document.getElementById("colorDisplay");
 colorDisplay.textContent = pickedColor;
 
 for(var i=0; i <squares.length; i++) {
+	//add initial color to squares
 	squares[i].style.background = colors[i];
+
+	//add click listeners to squares
+	squares[i].addEventListener("click", function() {
+		//grab color of clicked square
+		var clickedColor = this.style.background;
+		//compare color to picked color
+		if(clickedColor === pickedColor) {
+			alert("Correct!!" +"--"+ pickedColor+"-----"+clickedColor);			
+		} else {
+			alert("WRONG!!!" +"--"+ pickedColor+"-----"+clickedColor);
+		}
+
+
+	});
 
 };
